1716c6012877fc98cdff6d761389d77c0be58216
hdelery <hdelery@cisco.com>
2017-09-11 21:10:29 -0700

    Change CMX log file location

--- config/logger.js
+++ config/logger.js
@@ -2,7 +2,7 @@ var winston = require('winston');
 var logger = new (winston.Logger)({
     transports: [
       new (winston.transports.Console)({timestamp: true, level: 'info'})
-//      new (winston.transports.File)({timestamp: true, maxsize: 104857600, maxFiles: 10, level: 'info', handleExceptions: true, json: false,  filename: "/opt/cmx/var/log/cmx-api-server/server.log"})
+//      new (winston.transports.File)({timestamp: true, maxsize: 104857600, maxFiles: 10, level: 'info', handleExceptions: true, json: false,  filename: "/opt/cmx/var/log/apiserver/server.log"})
     ]
 });
 module.exports = logger;
